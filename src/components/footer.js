31896fe2437ce3c2b3ee0c4cc7bb4375f398c86a
muskan572 <muskanchhimpa2022@gmail.com>
2025-04-22 21:06:38 +0530

    adding the alt prop in the image tag

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,35 +1,35 @@
-export default function Footer({twitter, linkedin, youtube, facebook}){
-    return(
-        < div className="flex text-center justify-center flex-col ml-[15%] mt-[10%] mb-[10%] w-[70%]  bg-slate-50 rounded-3xl">
-            <div className="text-xl font-bold w-[20%] ">
-                <h1>FOODIE</h1>
-            </div>
-            <div className="flex flex-initial ml-[6%] mt-[2%]">
-                <img src={twitter} className="hover:shadow-md"/>
-                <img src={linkedin} className="hover:shadow-md"/>
-                <img src={youtube} className="hover:shadow-md"/>
-                <img src={facebook} className="hover:shadow-md"/>
-            </div>
-            <div className="flex flex-initial justify-center text-xl font-semibold text-stone-700  gap-5">
-            <div className="flex flex-col">
-                <a href="quality">Quality</a>
-                <a href="help">Help</a>
-                <a href="share">Share</a>
-                <a href="careers">Careers</a>
-                <a href="work">Work</a>
-                <a href="testimonials">Testimonials</a>
-            </div>
-            <div className="flex flex-col">
-                <h1>244-5333-7783</h1>
-                <a href="e-mail">hello@food.com</a>
-                <a href="e-mail">press@food.com</a>
-                <a href="e-mail">contact@food.com</a>
-            </div>
-            <div  iv className="flex flex-col">
-                <a href="Terms & Conditions">Terms & Conditions</a>
-                <a href="Privacy policy">Privacy policy</a>
-            </div>
-            </div>
+export default function Footer({ twitter, linkedin, youtube, facebook }) {
+  return (
+    <div className="flex text-center justify-center flex-col ml-[15%] mt-[10%] mb-[10%] w-[70%]  bg-slate-50 rounded-3xl">
+      <div className="text-xl font-bold w-[20%] ">
+        <h1>FOODIE</h1>
+      </div>
+      <div className="flex flex-initial ml-[6%] mt-[2%]">
+        <img src={twitter} className="hover:shadow-md" alt="twitter" />
+        <img src={linkedin} className="hover:shadow-md" alt="linkedin" />
+        <img src={youtube} className="hover:shadow-md" alt="youtube" />
+        <img src={facebook} className="hover:shadow-md" alt="fb" />
+      </div>
+      <div className="flex flex-initial justify-center text-xl font-semibold text-stone-700  gap-5">
+        <div className="flex flex-col">
+          <a href="quality">Quality</a>
+          <a href="help">Help</a>
+          <a href="share">Share</a>
+          <a href="careers">Careers</a>
+          <a href="work">Work</a>
+          <a href="testimonials">Testimonials</a>
         </div>
-    )
+        <div className="flex flex-col">
+          <h1>244-5333-7783</h1>
+          <a href="e-mail">hello@food.com</a>
+          <a href="e-mail">press@food.com</a>
+          <a href="e-mail">contact@food.com</a>
+        </div>
+        <div iv className="flex flex-col">
+          <a href="Terms & Conditions">Terms & Conditions</a>
+          <a href="Privacy policy">Privacy policy</a>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/testimonial.js
+++ src/components/testimonial.js
@@ -1,30 +1,37 @@
-export default function Testimonial({image, stars}){
-    return(
-        <div>  
-            <div className=" flex justify-center text-center mt-[10%]">
-                <div className="w-[45%] p-10">
-                    <div className="text-orange-400 text-2xl font-bold m-5">Testimonial</div>
-                    <h1 className="text-stone-700 text-5xl font-bold ">What They Are Saying</h1>
-                    <p className="text-stone-400 text-xl m-5">Lorem ipsum dolor sit amet consectetur. Non tincidunt 
-                        magna non et elit. Dolor  turpis molestie dui
-                        magnis facilisis at fringilla quam.</p>
-                </div>  
-            </div>
-        
-            <div className="text-center flex justify-center mb-3">
-                <div className="w-[50%] text-center bg-stone-200 rounded-3xl  hover:shadow-md">
-                    <div>
-                        <img src={image} className="w-[15%] p-4 ml-[40%]"/>
-                    </div>
-                    <div>
-                        <p className="text-stone-400 text-xl w-[70%] p-6 ml-[15%]">Lorem ipsum dolor sit amet consectetur. Non tincidunt 
-                        magna non et elit. Dolor  turpis molestie dui
-                        magnis facilisis at fringilla quam.</p>
-                        <img src={stars} className="ml-[32%]"/>
-                        <h1 className="text-2xl font-semibold mb-4 p-3">John Doe</h1>
-                    </div>
-                </div>
-            </div>
+export default function Testimonial({ image, stars }) {
+  return (
+    <div>
+      <div className=" flex justify-center text-center mt-[10%]">
+        <div className="w-[45%] p-10">
+          <div className="text-orange-400 text-2xl font-bold m-5">
+            Testimonial
+          </div>
+          <h1 className="text-stone-700 text-5xl font-bold ">
+            What They Are Saying
+          </h1>
+          <p className="text-stone-400 text-xl m-5">
+            Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+            elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+          </p>
         </div>
-    )
+      </div>
+
+      <div className="text-center flex justify-center mb-3">
+        <div className="w-[50%] text-center bg-stone-200 rounded-3xl  hover:shadow-md">
+          <div>
+            <img src={image} className="w-[15%] p-4 ml-[40%]" alt="avatar" />
+          </div>
+          <div>
+            <p className="text-stone-400 text-xl w-[70%] p-6 ml-[15%]">
+              Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+              elit. Dolor turpis molestie dui magnis facilisis at fringilla
+              quam.
+            </p>
+            <img src={stars} className="ml-[32%]" alt="stars" />
+            <h1 className="text-2xl font-semibold mb-4 p-3">John Doe</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
